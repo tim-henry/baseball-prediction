b5e64dc6a5de076aa434514c9f780c6e8a5f16bf
abishalom <abishalom1997@gmail.com>
2018-11-06 03:07:36 -0500

    Cumulative data cleaning, processed and ready for regression

--- util/Featurization.py
+++ util/Featurization.py
@@ -41,16 +41,33 @@ colNames = ['Date', 'GameNum', 'Day', 'Visitor_Name', 'Visitor_League', 'Visitor
             'Home_Starter8Name', 'Home_Starter8DefensivePosition', 'Home_Starter9ID', 'Home_Starter9Name',
             'Home_Starter9DefensivePosition', 'AdditionalInfo', 'AcquisitionInformation']
 
+regColNames =   ['Unnamed: 0', 'isWin', 'isHome', 'Name', 'GameNum', 'opp_Name', 'opp_GameNum', 'Runs', 'at-bats', 'hits', 'doubles', 'triples',
+                'Homeruns', 'RBI', 'SacrificeHits', 'hit-by-pitch', 'walks', 'intentionalWalks', 'strikeouts', 'StolenBases', 'CaughtStealing',
+                'GroundedIntoDoublePlays', 'AwardedFirstOnCatcherInterference', 'LeftOnBase', 'PitchersUsed', 'IndividualEarnedRuns',
+                'TeamEarnedRuns', 'WildPitches', 'Balks', 'putouts', 'assists', 'errors', 'PassedBalls', 'doublePlays', 'TriplePlays', 'opp_Runs',
+                'opp_at-bats', 'opp_hits', 'opp_doubles', 'opp_triples', 'opp_Homeruns', 'opp_RBI', 'opp_SacrificeHits', 'opp_hit-by-pitch',
+                'opp_walks', 'opp_intentionalWalks', 'opp_strikeouts', 'opp_StolenBases', 'opp_CaughtStealing', 'opp_GroundedIntoDoublePlays',
+                'opp_AwardedFirstOnCatcherInterference', 'opp_LeftOnBase', 'opp_PitchersUsed', 'opp_IndividualEarnedRuns', 'opp_TeamEarnedRuns',
+                'opp_WildPitches', 'opp_Balks', 'opp_putouts', 'opp_assists', 'opp_errors', 'opp_PassedBalls', 'opp_doublePlays', 'opp_TriplePlays',
+                'cum_isWin', 'cum_isHome', 'cum_GameNum', 'cum_Runs', 'cum_at-bats', 'cum_hits', 'cum_doubles', 'cum_triples',
+               'cum_Homeruns', 'cum_RBI', 'cum_SacrificeHits', 'cum_hit-by-pitch', 'cum_walks', 'cum_intentionalWalks', 'cum_strikeouts',
+               'cum_StolenBases', 'cum_CaughtStealing', 'cum_GroundedIntoDoublePlays', 'cum_AwardedFirstOnCatcherInterference', 'cum_LeftOnBase',
+               'cum_PitchersUsed', 'cum_IndividualEarnedRuns', 'cum_TeamEarnedRuns', 'cum_WildPitches', 'cum_Balks', 'cum_putouts', 'cum_assists',
+               'cum_errors', 'cum_PassedBalls', 'cum_doublePlays', 'cum_TriplePlays', 'opp_cum_isWin', 'opp_cum_isHome', 'opp_cum_GameNum',
+               'opp_cum_Runs', 'opp_cum_at-bats', 'opp_cum_hits', 'opp_cum_doubles', 'opp_cum_triples', 'opp_cum_Homeruns', 'opp_cum_RBI',
+               'opp_cum_SacrificeHits', 'opp_cum_hit-by-pitch', 'opp_cum_walks', 'opp_cum_intentionalWalks', 'opp_cum_strikeouts',
+               'opp_cum_StolenBases', 'opp_cum_CaughtStealing', 'opp_cum_GroundedIntoDoublePlays', 'opp_cum_AwardedFirstOnCatcherInterference',
+               'opp_cum_LeftOnBase', 'opp_cum_PitchersUsed', 'opp_cum_IndividualEarnedRuns', 'opp_cum_TeamEarnedRuns', 'opp_cum_WildPitches',
+               'opp_cum_Balks', 'opp_cum_putouts', 'opp_cum_assists', 'opp_cum_errors', 'opp_cum_PassedBalls', 'opp_cum_doublePlays',
+               'opp_cum_TriplePlays']
 
+cumColNames = [cn for cn in regColNames if 'cum' in cn]
 
 def drop_data(dropbox_dir, featured_dir):
     """
+    DEPRECATED. DON'T USE
     Here we erase statistics that are not game-level. Includes umpire information, pitcher/batter information,
     manager information. Want to focus purely on game-level stats for our model.
-
-    TODO: Instead of just saving these vanilla, want to create a single dataframe for each season. Make sure not
-    to double-count games by assigning a new column, gameID.
-        This column should be concatenation of Date, Visitor_name, home_name, game_num.
     """
 
     raw_data_dir = 'data_raw_team/'
@@ -84,41 +101,47 @@ def featurize_data(dropbox_dir, featured_dir, std=True):
     STD: boolean to control whether data is normalized to std. Gaussian or minmax normalization.
 
     """
-    to_one_hot = [1, 2, 3, 4, 5, 7, 8, 13, 15, 16, 17]
+    # to_one_hot = [1, 2, 3, 4, 5, 7, 8, 13, 15, 16, 17]
     # to_one_hot = [0, 1, 2, 3, 4, 6, 7, 12, 14, 15, 16] #list of categorical columns. From data description list
-
+    total = pd.DataFrame()
     for year in os.listdir(dropbox_dir+featured_dir):
         print("Working on files in: {}".format(year))
         for team in os.listdir(dropbox_dir + featured_dir + year):
             data_filename = dropbox_dir + featured_dir + year + '/' + team
             # print(data_filename)
             with open(data_filename) as f:
-                team_df = pd.read_csv(f, names=colNames)
+                team_df = pd.read_csv(f).dropna()
 
-                dropped_df = team_df.drop(np.array(colNames[to_one_hot]), axis=1)
-                vals_to_std = dropped_df.values
+                # dropped_df = team_df.drop(np.array(colNames[to_one_hot]), axis=1)
+                to_std = team_df[np.array(cumColNames)]
+                vals_to_std = to_std.values
                 if std:
                     scaler = preprocessing.StandardScaler()
                 else:
                     scaler = preprocessing.MinMaxScaler()
                 vals_scaled = scaler.fit_transform(vals_to_std)
-                std_df = pd.DataFrame(vals_scaled, columns = dropped_df.columns)
+                team_df[np.array(cumColNames)] = vals_scaled
+                # std_df = pd.DataFrame(vals_scaled, columns = to_std.columns)
+                # print(team_df.head(1))
+                total = total.append(team_df)
+                # team_df.to_csv(data_filename)
+    cols_to_keep = ['isWin', 'isHome'] + cumColNames
+    # print(list(total.columns))
+    # print(total[cols_to_keep])
+    total = total[cols_to_keep]
+    cols_to_drop = ['cum_isWin', 'cum_isHome', 'cum_GameNum',
+                    'opp_cum_isWin', 'opp_cum_isHome', 'opp_cum_GameNum']
+    total = total.drop(cols_to_drop)
+    total.to_csv(dropbox_dir + "CUM_CONCAT/CUM_CONCAT.csv")
 
-                team_df.to_csv(data_filename)
 
+def prepare_data(dropbox_dir, reclean = False, refeature = True):
 
-
-def prepare_data(dropbox_dir, reclean = True, refeature = True):
-
-    featured_dir = 'feature_data_team/'
+    featured_dir = 'CUM/'
     if reclean:
         drop_data(dropbox_dir, featured_dir)
     if refeature:
         featurize_data(dropbox_dir, featured_dir)
-
-
-
-
 
 
 
@@ -131,8 +154,14 @@ if __name__ == "__main__":
     # dropbox_dir = os.path.expanduser("~")
     # year = '/GL2017'
     # team = '/ANA.txt'
-    with open(dropbox_dir + "data_raw_team/GL1871/BS1.txt") as f:
-        team = pd.read_csv(f)
-        print(team.shape)
+    # with open(dropbox_dir + "data_raw_team/GL1871/BS1.txt") as f:
+    #     team = pd.read_csv(f)
+    #     print(team.shape)
     # print(os.listdir(dropbox_dir))
-    prepare_data(dropbox_dir, refeature = False)
+    prepare_data(dropbox_dir, reclean = False, refeature = True)
+    # f = open(dropbox_dir + 'CUM/DUMMY/ANA.csv')
+    # team = pd.read_csv(f)
+    # print(team.shape)
+    # print(team[cumColNames])
+    # print(cumColNames)
+
