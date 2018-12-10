264517093fafc0e48272b3133bab65dc79b5cc04
Timothy Henry <timhenry@mit.edu>
2018-12-10 16:46:52 -0500

    Updated LSTM to utilize player mixtures

--- src/lstm.py
+++ src/lstm.py
@@ -18,13 +18,15 @@ class Network(nn.ModuleList):
         super(Network, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
+        self.intermediate_dim = 8
         self.output_dim = output_dim
 
         self.batch_size = 1
 
         self.lstm = nn.LSTMCell(input_size=input_dim, hidden_size=hidden_dim)
-
-        self.output = nn.Linear(in_features=hidden_dim, out_features=output_dim)
+        self.fc = nn.Linear(in_features=hidden_dim, out_features=self.intermediate_dim)
+        # self.dropout = nn.Dropout(p=0.5)
+        self.output = nn.Linear(in_features=self.intermediate_dim, out_features=output_dim)
 
     """
     X: a sequence of vectors from previous game # for the teams of this matchup
@@ -39,7 +41,7 @@ class Network(nn.ModuleList):
 
             hidden_state, _ = hc
 
-            output_seq[i] = torch.sigmoid(self.output(hidden_state))
+            output_seq[i] = torch.sigmoid(self.output(self.fc(hidden_state)))
 
         return output_seq.view((sequence_size, -1))
 
@@ -54,6 +56,76 @@ def clean_file(full_name, year):
     n = raw_data.shape[1]
 
     # remove/reorganize columns
+    raw_data = raw_data.drop(columns=["Name", "opp_Name", "GameNum", "opp_GameNum", "cum_GameNum", "opp_cum_GameNum", "cum_isHome",
+                                      "opp_cum_isHome"])
+
+    to_drop = []
+    columns = [x for x in raw_data.columns if x.startswith("cum_") or x.startswith("batter") or x.startswith("pitcher")]
+    for column in columns:
+        raw_data[column] = raw_data[column] - raw_data["opp_" + column]
+
+    for i in range((57 - 1) // 2):
+        column = raw_data.columns[i + 2]
+        to_drop.append(column)
+        to_drop.append("opp_" + column)
+        to_drop.append("opp_cum_" + column)
+        to_drop.extend([y for y in raw_data.columns if y.startswith("opp_pitcher") or y.startswith("opp_batter")])#["opp_pitcher_0", "opp_pitcher_1", "opp_batter_0", "opp_batter_1"])
+    raw_data = raw_data.drop(columns=to_drop)
+    raw_data = raw_data.dropna(axis=0)
+
+    # raw_data.to_csv(year + "out.csv", index=False)
+
+    return raw_data.drop(columns=["isWin"]).values, raw_data[["isWin"]].values
+
+# Clean Data
+def clean_file_shifted_player(full_name, year):
+    raw_data = pd.read_csv(full_name)
+    # raw_data = dropLowGames(raw_data, threshold=80)
+    n = raw_data.shape[1]
+
+    # remove/reorganize columns
+    raw_data = raw_data.drop(columns=["Name", "opp_Name", "GameNum", "opp_GameNum", "cum_isWin",
+                                      "opp_cum_isWin", "cum_GameNum", "opp_cum_GameNum", "cum_isHome",
+                                      "opp_cum_isHome"])
+
+    to_drop = []
+    columns = [x for x in raw_data.columns if not x.startswith("opp_") and not x.startswith("cum_")
+               and x != "isWin" and x != "isHome"]
+
+    for i in range((57 - 1) // 2):
+        column = raw_data.columns[i + 2]
+        raw_data["cum2_" + column] = raw_data["cum_" + column].shift(-1)
+
+    for i in range((57 - 1) // 2):
+        column = raw_data.columns[i + 2]
+        raw_data["opp_cum2_" + column] = raw_data["opp_cum_" + column].shift(-1)
+
+    for i in range((57 - 1) // 2):
+        column = raw_data.columns[i + 2]
+        raw_data["cum_" + column] = - raw_data["opp_cum_" + column] + raw_data["cum_" + column]
+        raw_data["cum2_" + column] = - raw_data["opp_cum2_" + column] + raw_data["cum2_" + column]
+        to_drop.append("opp_cum_" + column)
+        to_drop.append("opp_cum2_" + column)
+
+    for column in columns:
+        raw_data[column] = - raw_data["opp_" + column] + raw_data[column]
+        to_drop.append("opp_" + column)
+
+
+    raw_data = raw_data.drop(columns=to_drop)
+
+    raw_data['isWin'] = raw_data['isWin'].shift(-1)
+
+    raw_data = raw_data.dropna(axis=0)
+
+    return raw_data.drop(columns=["isWin"]).values, raw_data[["isWin"]].values
+
+# Clean Data
+def clean_file_shifted(full_name, year):
+    raw_data = pd.read_csv(full_name)
+    n = raw_data.shape[1]
+
+    # remove/reorganize columns
     raw_data = raw_data.drop(columns=["Unnamed: 0", "Name", "opp_Name", "GameNum", "opp_GameNum", "cum_isWin",
                                       "opp_cum_isWin", "cum_GameNum", "opp_cum_GameNum", "cum_isHome",
                                       "opp_cum_isHome"])
@@ -61,16 +133,84 @@ def clean_file(full_name, year):
     to_drop = []
     for i in range((57 - 1) // 2):
         column = raw_data.columns[i + 2]
-        raw_data[column] = raw_data[column] - raw_data["opp_" + column]
+        raw_data["cum2_" + column] = raw_data["cum_" + column].shift(-1)
+
+    for i in range((57 - 1) // 2):
+        column = raw_data.columns[i + 2]
+        raw_data["opp_cum2_" + column] = raw_data["opp_cum_" + column].shift(-1)
+
+    for i in range((57 - 1) // 2):
+        column = raw_data.columns[i + 2]
+        raw_data[column] = - raw_data["opp_" + column] + raw_data[column]
         raw_data["cum_" + column] = - raw_data["opp_cum_" + column] + raw_data["cum_" + column]
+        raw_data["cum2_" + column] = - raw_data["opp_cum2_" + column] + raw_data["cum2_" + column]
+
         to_drop.append("opp_" + column)
         to_drop.append("opp_cum_" + column)
+        to_drop.append("opp_cum2_" + column)
+
     raw_data = raw_data.drop(columns=to_drop)
+
+    raw_data['isWin'] = raw_data['isWin'].shift(-1)
+
     raw_data = raw_data.dropna(axis=0)
 
-    raw_data['year'] = int(year[2:])
-
     return raw_data.drop(columns=["isWin"]).values, raw_data[["isWin"]].values
+
+
+# Clean Data
+def clean_file_shifted_team(full_name, year):
+    raw_data = pd.read_csv(full_name)
+    team_data = pd.read_csv(dropbox_dir + "teams-one-hot.csv", index_col=0)
+
+    n = raw_data.shape[1]
+
+    team_features = pd.DataFrame(index=raw_data.index, columns=team_data.columns)
+    opp_features = pd.DataFrame(index=raw_data.index, columns=team_data.columns)
+
+    # print(raw_data.get_dtype_counts(), team_features.get_dtype_counts(), opp_features.get_dtype_counts())
+    for idx in range(raw_data.shape[0]):
+        team_features.at[idx] = team_data.loc[raw_data.at[idx, "Name"]]
+        opp_features.at[idx] = team_data.loc[raw_data.at[idx, "opp_Name"]]
+    # opp_features = opp_features.add_prefix("opp_")
+    # temp = team_features.astype('float64').join(opp_features.astype('float64'))
+    # raw_data = pd.merge(raw_data, temp, how='left', left_on=raw_data.index, right_on=temp.index)
+    # raw_data = raw_data.join(team_features.astype('float64')).join(opp_features.astype('float64'))# = pd.concat([raw_data, team_features.astype('float64'), opp_features.astype('float64')], sort=False)
+    # print(raw_data)
+    raw_data = raw_data.join(team_features.astype('float64').sub(opp_features.astype('float64')))
+
+    # remove/reorganize columns
+    raw_data = raw_data.drop(columns=["Unnamed: 0", "Name", "opp_Name", "GameNum", "opp_GameNum", "cum_isWin",
+                                      "opp_cum_isWin", "cum_GameNum", "opp_cum_GameNum", "cum_isHome",
+                                      "opp_cum_isHome"])
+
+    to_drop = []
+    for i in range((57 - 1) // 2):
+        column = raw_data.columns[i + 2]
+        raw_data["cum2_" + column] = raw_data["cum_" + column].shift(-1)
+
+    for i in range((57 - 1) // 2):
+        column = raw_data.columns[i + 2]
+        raw_data["opp_cum2_" + column] = raw_data["opp_cum_" + column].shift(-1)
+
+    for i in range((57 - 1) // 2):
+        column = raw_data.columns[i + 2]
+        raw_data[column] = - raw_data["opp_" + column] + raw_data[column]
+        raw_data["cum_" + column] = - raw_data["opp_cum_" + column] + raw_data["cum_" + column]
+        raw_data["cum2_" + column] = - raw_data["opp_cum2_" + column] + raw_data["cum2_" + column]
+
+        to_drop.append("opp_" + column)
+        to_drop.append("opp_cum_" + column)
+        to_drop.append("opp_cum2_" + column)
+
+    raw_data = raw_data.drop(columns=to_drop)
+
+    raw_data['isWin'] = raw_data['isWin'].shift(-1)
+    raw_data['year'] = int(year[2:]) - 1975
+
+    raw_data = raw_data.dropna(axis=0)
+    return raw_data.drop(columns=["isWin"]).values, raw_data[["isWin"]].values
+
 
 # Clean Data w/ team vectors
 def clean_file_team(full_name, year):
@@ -100,14 +240,17 @@ def clean_file_team(full_name, year):
     to_drop = []
     for i in range((57 - 1) // 2):
         column = raw_data.columns[i + 2]
-        raw_data[column] = raw_data[column] - raw_data["opp_" + column]
-        raw_data["cum_" + column] = - raw_data["opp_cum_" + column] + raw_data["cum_" + column]
+        # raw_data[column] = raw_data[column] - raw_data["opp_" + column]
+        # raw_data["cum_" + column] = - raw_data["opp_cum_" + column] + raw_data["cum_" + column]
+        to_drop.append(column)
         to_drop.append("opp_" + column)
+        to_drop.append("cum_" + column)
         to_drop.append("opp_cum_" + column)
     raw_data = raw_data.drop(columns=to_drop)
     raw_data = raw_data.dropna(axis=0)
 
-    raw_data['year'] = int(year[2:]) - 1975
+    # raw_data['year'] = int(year[2:])
+    # print(raw_data.size, raw_data.columns)
     return raw_data.drop(columns=["isWin"]).values, raw_data[["isWin"]].values
 
 # Load Data
@@ -122,7 +265,7 @@ def get_data(in_dir, year):
             # ignore hidden files, etc.
             if ext.lower() != "txt" and ext.lower() != "csv":
                 continue
-            seq, label = clean_file_team(full_name, year)
+            seq, label = clean_file(full_name, year)
             if np.isnan(seq).any() or np.isnan(label).any():
                 # print(year, team_name)
                 print("WARNING: nan's found: ", year, team_name)
@@ -139,6 +282,18 @@ def get_acc(y_pred, y, burn_index=0):
     correct = torch.sum(y_pred_rnd[burn_index:] == y.long()[burn_index:])
     return correct.item(), tot
 
+def dropLowGames(df, colStart = 125, threshold = 5):
+    new_df = df.iloc[:, colStart:]
+
+    print('Thresholding')
+
+    new_df = df.drop([col for col, val in new_df.iloc[:, colStart:].abs().sum().iteritems()
+             if val < threshold], axis=1)
+
+    numPlayersRemoved = (len(df.columns) - (len(new_df.columns)))
+
+    print('Number of Players Removed: ' + str(numPlayersRemoved))
+    return(new_df)
 
 # ==========================================================================================================
 
@@ -155,20 +310,20 @@ if __name__ == "__main__":
     
     # Data
     dropbox_dir = "/Users/timhenry/Dropbox (MIT)/6.867/"
-    in_dir = dropbox_dir + "data_clean_csv_wins_cumulated"
-    train_seasons = [x for x in range(1921, 2012)]
-    test_seasons = [x for x in range(2013, 2017)]
-    input_dim = 200
-
+    in_dir = dropbox_dir + "data_clean_csv_wins_cumulated_withplayers_transformed/pca/num_degrees5"
+    train_seasons = [x for x in range(2010, 2015)]
+    test_seasons = [x for x in range(2015, 2017)]
+    input_dim = 41
     # Hyper-params
-    hidden_dim = 15  # TODO ?
+    hidden_dim = 16  # TODO ?
     output_dim = 1  # classification
     model = Network(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim)
     model = model.to(device)
 
-    criterion = nn.BCELoss()  # TODO custom loss function? (burn first n predictions?)
-    lr = 0.01
-    optimizer = optim.Adam(model.parameters(), lr=lr)
+    criterion = nn.BCELoss()
+    lr = 0.02
+    optimizer = optim.Adam(model.parameters(), weight_decay=1e-3, lr=lr)
+    # scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min", patience=2)
 
     # Train
     print("============== Training ==============")
@@ -185,7 +340,7 @@ if __name__ == "__main__":
             season_correct = 0
             season_total = 0
             for seq_idx in range(len(train_X)):
-                X, Y = torch.from_numpy(train_X[seq_idx][:-1]), torch.from_numpy(train_Y[seq_idx][1:])
+                X, Y = torch.from_numpy(train_X[seq_idx]), torch.from_numpy(train_Y[seq_idx])
                 X = X.to(device)
                 Y = Y.to(device)
 
@@ -195,12 +350,13 @@ if __name__ == "__main__":
                 loss.backward()
 
                 optimizer.step()
+                # scheduler.step(loss)
                 num_correct, num_total = get_acc(Y_pred, Y)
                 # print("Acc for season {0}, team {1}: {2:3.3f}".format(season, seq_idx, num_correct / num_total))
                 season_correct += num_correct
                 season_total += num_total
 
-            print("Season {0} accuracy: {1:3.3f}".format(season, season_correct / season_total))
+            # print("Season {0} accuracy: {1:3.3f}".format(season, season_correct / season_total))
             total_correct += season_correct
             total += season_total
 
@@ -212,20 +368,20 @@ if __name__ == "__main__":
     model.eval()
     total_correct = 0
     total = 0
-    for season in test_seasons:# TODO
+    for season in test_seasons:
         test_X, test_Y = get_data(in_dir, "GL" + str(season))
 
         season_correct = 0
         season_total = 0
         for seq_idx in range(len(test_X)):
-            X, Y = torch.from_numpy(test_X[seq_idx][:-1]), torch.from_numpy(test_Y[seq_idx][1:])
+            X, Y = torch.from_numpy(test_X[seq_idx]), torch.from_numpy(test_Y[seq_idx])
             X = X.to(device)
             Y = Y.to(device)
 
             Y_pred = model(X, model.init_hidden())
 
             num_correct, num_total = get_acc(Y_pred, Y)
-            print("Acc for season {0}, team {1}: {2:3.3f}".format(season, seq_idx, num_correct / num_total))
+            # print("Acc for season {0}, team {1}: {2:3.3f}".format(season, seq_idx, num_correct / num_total))
             season_correct += num_correct
             season_total += num_total
 
